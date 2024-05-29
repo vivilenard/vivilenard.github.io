7c0cb9b44946c097fe1a64852e59059b7498e91d
vivi <vivi>
2024-05-29 13:06:43 +0000

    shifted all files in docs

---
+++ docs/js/coloreffect.js
@@ -0,0 +1,6 @@
+document.getElementById("colorful-background").addEventListener("pointermove", (e)=>{
+    const { currentTarget: el, clientX: x, clientY: y } = e;
+    const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+    el.style.setProperty('--posX',  x-l-w/2);
+    el.style.setProperty('--posY',  y-t-h/2);
+  })
